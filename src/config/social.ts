2170ed92e5bf1ea3cb9a986088c47d75eba45ad9
Jin Yeh <jjyeh23@gmail.com>
2026-08-18 13:32:07 -0700

    Update social.ts

--- src/config/social.ts
+++ src/config/social.ts
@@ -3,33 +3,33 @@ import type { SocialLink } from "../types";
 export const SOCIALS: SocialLink[] = [
     {
         name: "Github",
-        href: "https://github.com/shannon",
-        linkTitle: `Follow Claude Shannon on Github`,
+        href: "https://github.com/jjyeh23",
+        linkTitle: `Follow Jin Yeh on Github`,
         isActive: true,
     },
     {
         name: "Mail",
-        href: "mailto:shannon@bell-labs.com",
-        linkTitle: `Send an email to Claude`,
+        href: "jjyeh9 at uw dot edu",
+        linkTitle: `Send an email to Jin`,
         isActive: true,
     },
     {
         name: "Google Scholar",
         href: "https://scholar.google.com/citations?user=shannon",
-        linkTitle: `Claude Shannon on Google Scholar`,
-        isActive: true,
+        linkTitle: `Jin Yeh on Google Scholar`,
+        isActive: false,
     },
     {
         name: "ORCID",
-        href: "https://orcid.org/0000-0002-1825-0097",
-        linkTitle: `Claude Shannon on ORCID`,
+        href: "https://orcid.org/0009-0004-9808-8529",
+        linkTitle: `Jin Yeh on ORCID`,
         isActive: true,
     },
     {
         name: "LinkedIn",
-        href: "https://www.linkedin.com/in/claude-shannon-123456789/",
-        linkTitle: `Claude Shannon on LinkedIn`,
-        isActive: true, // Assuming Claude doesn't have a LinkedIn profile
+        href: "https://www.linkedin.com/in/jin-yeh/",
+        linkTitle: `Jin Yeh on LinkedIn`,
+        isActive: true, //
     },
 ];
 
